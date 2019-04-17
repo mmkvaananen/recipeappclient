2b2f591b66b2afa4a78a6e18e4d8a387ba8e8cc7
MariV <mmk.vaananen@gmail.com>
2019-04-17 17:36:25 +0300

    Recipeinfo component first version ready, shows recipe ingredients

--- src/assetComponents/RecipeIngredientsTable.js
+++ src/assetComponents/RecipeIngredientsTable.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react'
 export default class RecipeIngredientsTable extends Component {
     render() {
         return (
-            <div>
-            
-            </div>
+            <tr>
+            <td>{this.props.number}</td>
+            <td>{this.props.name}</td>
+            <td>{this.props.amount}</td>
+            <td>{this.props.unit}</td>
+            </tr>
         )
     }
 }
--- src/pages/RecipeInfo.js
+++ src/pages/RecipeInfo.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import {Table} from 'react-bootstrap';
 import {getRecipeIngredientsByRecipeId} from '../service/recipeFunctions';
+import RecipeIngredientsTable from '../assetComponents/RecipeIngredientsTable';
 
 export default class RecipeInfo extends Component {
 
@@ -27,9 +29,32 @@ export default class RecipeInfo extends Component {
     render() {
         console.log("RecipeInfo render, recipe id from url: ", this.props.match.params);
         console.log("this.state.ingredients:", this.state.ingredients);
+        let ingredientsList = this.state.ingredients;
+
+        ingredientsList = ingredientsList.map((row, index) => {
+            console.log("RecipeInfo mapRow: ", row);
+            let ingredientsNumber = index+1;
+            return <RecipeIngredientsTable key={index} number={ingredientsNumber} name={row.ingredient.name} amount={row.amount} unit={row.ingredient.servingUnit} />
+        })
+
         return (
             <div>
             recipeinfo page... to be implemented...
+
+                <Table striped bordered hover variant="dark">
+                    <thead>
+                        <tr><th>{this.props.match.params.recipename}</th></tr>
+                        <tr>
+                        <th>#</th>
+                        <th>Ingredient</th>
+                        <th>amount</th>
+                        <th>Units</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {ingredientsList ? ingredientsList : null}
+                    </tbody>
+                </Table>
             </div>
         )
     }
